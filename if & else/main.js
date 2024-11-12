650f85c694e6a57355c654c830c52682b0a4fcc4
tariq765 <tariqasghar761@gmail.com>
2024-11-12 15:31:05 +0500

    Add files via upload

---
+++ if & else/main.js
@@ -0,0 +1,55 @@
+/*let condition: boolean = false;
+
+
+
+if (condition) {
+    console.log("ghar par rahain");
+
+}
+    else {
+        console.log("barish injoy karain");
+        
+    }
+ let condino2 : boolean =false;
+ if(condino2){
+    console.log("tarq acha bacha hai");
+}
+else{
+    console.log("tariq ganda bacha hai");
+    
+}*/
+console.log("Ibrahim Ali Bhai");
+var studtent = {
+    name: "nadeem",
+    class: 9,
+    ROLL: 55,
+};
+console.log(studtent);
+var physicMarks = 85;
+var chemistryMarks = 86;
+var mathMark = 87;
+var obtainedMark = physicMarks + chemistryMarks + mathMark;
+var totalMarks = 300;
+var percentage = obtainedMark / totalMarks * 100;
+console.log("physics marks ", physicMarks);
+console.log("chemistry marks", chemistryMarks);
+console.log("math marks", mathMark);
+console.log("obtained marks", obtainedMark);
+console.log("total marks", totalMarks);
+console.log("percentage", percentage.toFixed(2) + "%");
+if (percentage >= 80) {
+    console.log("Grade A+");
+}
+else if (percentage >= 70) {
+    console.log("Grade A");
+}
+else if (percentage >= 60) {
+    console.log("Grade B");
+}
+else if (percentage >= 50) {
+    console.log("Grade C");
+}
+else if (percentage >= 40) {
+    console.log("Resul fail");
+}
+;
---
+++ if & else/main.ts
@@ -0,0 +1,88 @@
+
+
+
+
+/*let condition: boolean = false;
+
+
+
+if (condition) {
+    console.log("ghar par rahain");
+
+}
+    else {
+        console.log("barish injoy karain");
+        
+    }
+ let condino2 : boolean =false;
+ if(condino2){
+    console.log("tarq acha bacha hai");
+}
+else{
+    console.log("tariq ganda bacha hai");
+    
+}*/
+console.log("Ibrahim Ali Bhai");
+
+
+let studtent :{
+    name:string,
+    class:number,
+    ROLL:number,
+}=
+
+ {
+    name:"nadeem",
+    class:9,
+    ROLL:55,
+}
+console.log(studtent);
+
+
+let physicMarks : number       = 85;
+let chemistryMarks: number     = 86;
+let mathMark: number           = 87;
+let obtainedMark: number       =physicMarks+chemistryMarks+mathMark;
+let totalMarks:number          =300;
+let percentage: number         =obtainedMark/totalMarks*100
+
+
+
+console.log("physics marks " ,physicMarks);
+console.log("chemistry marks",chemistryMarks);
+console.log("math marks"     ,mathMark);
+console.log("obtained marks" ,obtainedMark);
+console.log("total marks"   ,totalMarks);
+console.log("percentage"     ,percentage.toFixed(2)+"%");
+
+
+if (percentage >=80){
+    console.log("Grade A+");
+    }
+else if(percentage >=70){
+    console.log("Grade A");
+    }
+else if (percentage >=60){
+    console.log("Grade B");}
+    else if(percentage >=50){
+        console.log("Grade C");}
+        else if (percentage >=40) 
+        {
+            console.log("Resul fail");
+        }; 
+            
+        
+    
+    
+
+    
+     
+
+
+
+
+
+
+
+
+
